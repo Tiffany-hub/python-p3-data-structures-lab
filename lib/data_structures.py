9fe1fe710b7522ce5d430d291488f1ed38b1e346
+ <+>
2023-08-24 12:00:08 +0300

    completed assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,48 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return next((food for food in spicy_foods if food["cuisine"] == cuisine), None)
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    return total_heat // num_foods if num_foods > 0 else 0
+
+# Call the functions and print the results
+names = get_names(spicy_foods)
+print("Names:", names)
+
+spiciest_foods = get_spiciest_foods(spicy_foods)
+print("Spiciest Foods:", spiciest_foods)
+
+print("All Spicy Foods:")
+print_spicy_foods(spicy_foods)
+
+american_food = get_spicy_food_by_cuisine(spicy_foods, "American")
+print("American Food:", american_food)
+
+print("Spiciest Foods:")
+print_spiciest_foods(spicy_foods)
+
+average_heat = get_average_heat_level(spicy_foods)
+print("Average Heat Level:", average_heat)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_spicy_foods = spicy_foods.copy()  # Create a copy to avoid modifying the original list
+    new_spicy_foods.append(spicy_food)    # Add the new spicy_food dictionary
+    return new_spicy_foods
